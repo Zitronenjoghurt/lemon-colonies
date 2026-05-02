cf5421dc6b5b3799bafeac38374f12aa22a174fb
Zitronenjoghurt <117850085+Zitronenjoghurt@users.noreply.github.com>
2026-05-02 17:53:54 +0200

    add subscribed chunk area metric

--- server/src/websocket/chunk_subscriptions.rs
+++ server/src/websocket/chunk_subscriptions.rs
@@ -11,13 +11,21 @@ pub struct ChunkSubscriptions {
 impl ChunkSubscriptions {
     /// Returns the previous rect if there is one.
     pub fn subscribe(&self, id: ConnectionId, rect: Rect<i32>) -> Option<Rect<i32>> {
+        metrics::gauge!("ws.subscribed_chunk_area").increment(rect.area() as f64);
+
         let prev = self.subscriptions.get(&id).map(|r| *r);
+        if let Some(prev) = prev {
+            metrics::gauge!("ws.subscribed_chunk_area").decrement(prev.area() as f64);
+        }
+
         self.subscriptions.insert(id, rect);
         prev
     }
 
     pub fn unsubscribe(&self, id: ConnectionId) {
-        self.subscriptions.remove(&id);
+        if let Some((_, rect)) = self.subscriptions.remove(&id) {
+            metrics::gauge!("ws.subscribed_chunk_area").decrement(rect.area() as f64);
+        }
     }
 
     pub fn connections_for_chunk(&self, coords: ChunkCoords) -> Vec<ConnectionId> {
